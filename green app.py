00c9a5945956723e967923a5bb61470c93473731
JAYDISH KENNEDY J <jaydishkennedy@gmail.com>
2025-09-03 22:21:53 +0530

    Update green app.py

--- green app.py
+++ green app.py
@@ -125,7 +125,7 @@ html_code = """
 <body>
   <!-- Header Section -->
   <header>
-    <img src="https://images.pexels.com/photos/13942394/pexels-photo-13942394.jpeg" class="logo" alt="Our Lady of Fatima">
+    <img src="https://drive.google.com/uc?export=view&id=1UmuM37kxb8qslHnmLP7fAUnDw0R2zxxf" class="logo" alt="Our Lady of Fatima">
     <h1>SMC STORES</h1>
     <p>BY DEPT OF BOTANY, ST. MARY'S COLLEGE (TUTICORIN)</p>
   </header>
