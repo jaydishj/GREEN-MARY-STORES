8853611b102088ba9f4431bf7d6d96b9b9fc0037
JAYDISH KENNEDY J <jaydishkennedy@gmail.com>
2025-12-03 22:24:25 +0530

    Update green app.py

--- green app.py
+++ green app.py
@@ -179,7 +179,7 @@ header p {
 </html>
 """
 
-components.html(html_code, height=900, scrolling=True)
+components.html(html_code, height=800, scrolling=True)
 # ------------------ DATABASE ------------------
 DB_FILE = "orders.db"
 
